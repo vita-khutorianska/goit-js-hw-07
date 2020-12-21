550447be077dc8658b1d7fb8ba18606a90b0dcb8
Vita Khutorianska <steblvita@gmail.com>
2020-12-21 09:57:35 +0200

    fix task 5 and 6

--- js/task-5.js
+++ js/task-5.js
@@ -1,14 +1,19 @@
-const inputRef = document.querySelector('#name-input');
-const outputRef = document.querySelector('#name-output');
+// const inputRef = document.querySelector('#name-input');
+// const outputRef = document.querySelector('#name-output');
 
-function changeInputValue(event) {
-    inputRef.value ? outputRef.textContent = event.currentTarget.value : outputRef.textContent = event.target.value;
-}
-inputRef.addEventListener('input', changeInputValue);
+// function changeInputValue(event) {
+//     inputRef.value ? outputRef.textContent = event.currentTarget.value : outputRef.textContent = event.target.value;
+// }
+// inputRef.addEventListener('input', changeInputValue);
 
 
 // inputRef.addEventListener('input', event => { inputRef.value ? outputRef.textContent = event.currentTarget.value : outputRef.textContent = event.target.value });
 
 
-
+const inputRef = document.querySelector('#name-input');
+const outputRef = document.querySelector('#name-output');
+inputRef.addEventListener('input', checkInputText)
+function checkInputText(event) {  
+ outputRef.textContent = event.target.value === "" ?  'незнакомец': event.target.value; 
+ }
 
--- js/task-6.js
+++ js/task-6.js
@@ -1,17 +1,16 @@
 
+
 const inputRef = document.querySelector('#validation-input');
 
-function inputHandlerBlur() {
-  if (
-    inputRef.value.length > Number(inputRef.dataset.length) ||
-    inputRef.value.length < Number(inputRef.dataset.length)
-  ) {
-    inputRef.classList.add('invalid');
-    inputRef.classList.remove('valid');
-  } else if (inputRef.value.length === Number(inputRef.dataset.length)) {
-    inputRef.classList.add('valid');
-    inputRef.classList.remove('invalid');
+inputRef.addEventListener('blur', inputHandlerBlur)
+
+function inputHandlerBlur(event) {  
+ 
+  if (event.target.value.length === Number(inputRef.dataset.length)) {   
+    inputRef.classList.add("valid");
+       inputRef.classList.remove("invalid")
+  } else {
+    inputRef.classList.add("invalid");
+    inputRef.classList.remove("valid")
   }
 }
-inputRef.addEventListener('blur', inputHandlerBlur);
-
